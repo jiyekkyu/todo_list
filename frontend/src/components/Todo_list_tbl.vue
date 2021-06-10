523981be35c5ed637ad26297789032fac61354c9
leeji5670 <leeji3063@naver.com>
2021-06-10 10:27:43 +0900

    Todo 리스트 테이블 코드 변경

---
+++ frontend/src/components/Todo_list_tbl.vue
@@ -0,0 +1,30 @@
+<template>
+	<li class="li-st">
+		<input type="checkbox" 
+			   :id="'todo-chk'+row.idx" 
+			   v-model="row.checked" 
+			   true-value=1 
+			   false-value=0 
+			   @change="todoSet(row)">
+		<label :for="'todo-chk'+row.idx">
+			<span :class="row.checked == 1 ? 'checkmark' : ''"></span>
+		</label>
+		{{row.todo}}
+	</li>
+</template>
+
+<script>
+export default {
+	name: "Todolist",
+	props: {
+		row: Object,
+	},
+	methods: {
+		async todoSet(url, data) {
+			await this.$axios.post('/todo/checkup', {
+				data
+			});
+		},
+	},
+};
+</script>
--- frontend/src/views/Todo_list.vue
+++ frontend/src/views/Todo_list.vue
@@ -5,32 +5,11 @@
 				<div class="title">To-do List</div>
 				<div class="todo-add">
 					<input type="text" class="todo-input" placeholder="New Todo" v-model="todo">
-					<button class="add-btn" @click="todoSet('todo', todo)"> Add </button>
+					<button class="add-btn" @click="todoSet(todo)"> Add </button>
 				</div>
-				<table class="tb">
-					<thead>
-						<tr>
-							<td>State</td>
-							<td style="width: 100%">Todo</td>
-						</tr>
-					</thead>
-					<tbody>
-						<tr v-for="(row, i) in rows">
-							<td>
-								<input type="checkbox"
-								:id="'todo-chk'+i"
-								v-model="row.checked"
-								true-value=1
-								false-value=0
-								@change="todoSet('todo/checkUp', row)">
-								<label :for="'todo-chk'+i">
-									<span :class="row.checked == 1 ? 'checkmark' : ''"></span>
-								</label>
-							</td>
-							<td>{{row.todo}}</td>
-						</tr>
-					</tbody>
-				</table>
+				<ul class="ul-st">
+					<todolist-table v-for="row in rows" :key="row" :row="row"></todolist-table>
+				</ul>
 			</div>
 		</section>
 	</div>
@@ -51,8 +30,8 @@ export default {
 
 			this.rows = data;
 		},
-		async todoSet(url, data) {
-			await this.$axios.post('/'+url, {
+		async todoSet(data) {
+			await this.$axios.post('/todo', {
 				data
 			});
 		},
