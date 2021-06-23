282e3c0dc8f9b54a6ffff943289eda50506ea079
leeji5670 <leeji3063@naver.com>
2021-06-23 16:35:43 +0900

    frontend/docs -> ./docs

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,5 @@ const path = require('path');
 module.exports = {
 	lintOnSave: true,
 	publicPath: '/todo_list',
-	outputDir: path.resolve(__dirname,'./docs'),
+	outputDir: path.resolve(__dirname,'../docs'),
 };
